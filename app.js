059882f49611ed288714e7f9498b5b96df96787e
LuishgFarias <luishgfarias@gmail.com>
2023-01-27 12:17:52 -0300

    update CORS

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
+    app.use(cors({
+      origin: 'https://smat-food.vercel.app/'
+    }));
     next();
   });
   app.use(express.json())
