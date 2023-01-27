289bfe1c9aebcb0cea3bca3fa79c6a495930f02c
LuishgFarias <luishgfarias@gmail.com>
2023-01-27 12:30:08 -0300

    cors

--- app.js
+++ app.js
@@ -15,13 +15,12 @@ const app = express();
 
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", ['https://smat-food.vercel.app',
-    'https://smat-food.vercel.app/registro']);
+    res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Methods", "*");
     app.use(cors({
-      origin: ['https://smat-food.vercel.app',
-        'https://smat-food.vercel.app/registro']
+      origin: "*",
+        methods: "*"
     }));
     next();
   });
