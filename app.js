943e897a531f1bf6ccd572026325000a8efa0140
LuishgFarias <luishgfarias@gmail.com>
2023-01-27 12:38:07 -0300

    cors

--- app.js
+++ app.js
@@ -13,18 +13,6 @@ const Estabelecimento = require("./models/location");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  next();
-});
 app.use(express.json());
 
 app.post("/", (req, res) => {
